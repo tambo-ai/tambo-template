bbd79cfc847089807c5cc43f89261d127c081c2b
michael <mike.michaelmilstead@gmail.com>
2025-04-01 10:03:52 -0700

    update useeffect dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 import { ExampleComponent } from "@/components/example-component";
 import { MessageThreadFull } from "@/components/ui/message-thread-full";
+import { WelcomeCard } from "@/components/welcome-card";
 import { useTambo } from "@tambo-ai/react";
 import { useEffect } from "react";
-import { WelcomeCard } from "@/components/welcome-card";
 
 export default function Home() {
   const { registerComponent, thread } = useTambo();
@@ -26,13 +26,13 @@ export default function Home() {
         inStock: "boolean"
       } // Here we tell tambo what props the component expects
     });
-  }, [registerComponent, thread]);
+  }, [registerComponent]);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         {(!thread || thread.messages.length === 0) && <WelcomeCard />}
-        <MessageThreadFull contextKey="tambo-template"/>
+        <MessageThreadFull contextKey="tambo-template" />
       </main>
     </div>
   );
