a50883ef2fdb3ba0129fec0e07f826f3920b5936
Michael Milstead <mike.michaelmilstead@gmail.com>
2025-04-25 12:42:37 -0700

    fix: remove component-tool associations (#15)
    * remove 'tool association'
    
    * bump tambo-ai/react version
    
    * bump tambo-ai/react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 import { MessageThreadFull } from "@/components/ui/message-thread-full";
+import { productsTool } from "@/lib/tambo";
+import { useTambo } from "@tambo-ai/react";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { registerTool } = useTambo();
+
+  useEffect(() => {
+    registerTool(productsTool);
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
